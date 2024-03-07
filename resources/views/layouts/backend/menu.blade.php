a8c8f674b1ebf29fcf8fb412e9e0030a8e817aa5
moominwa <pond_benpolo@hotmail.com>
2024-03-07 10:28:57 +0700

    hellooo

--- resources/views/layouts/backend/menu.blade.php
+++ resources/views/layouts/backend/menu.blade.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: rgb(103, 130, 199)">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{route("dashboard")}}" class="brand-link">
       <img src="asset/backend/dist/img/hello.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Admin</span>
     </a>
@@ -54,6 +54,26 @@
                 <a href="{{route("project")}}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Team</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{route("project")}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>จัดการอนุมัติ</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="{{route("project")}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>จัดการตารางแข่ง</p>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="{{route("project")}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>จัดการผลการแข่ง</p>
                 </a>
               </li>
 
